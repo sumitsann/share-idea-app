ececad1d5eaa3b3c9a6be9dd886c362997180a22
Sumit Ojha <sumitojha501@gmail.com>
2022-10-25 17:25:18 -0500

    Updated config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://mainuser:PNaTuLdsek6C0OxU@cluster0.by3ygg8.mongodb.net/ideasDB?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
